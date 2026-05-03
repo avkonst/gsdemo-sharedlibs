831e14f6cc602a31197d74ed4b58f242b127f989
Andrey Konstantinov <avkonst@gmail.com>
2026-05-03 22:44:18 +0000

    turn into workspace

--- libs/logging/src/lib.rs
+++ libs/logging/src/lib.rs
@@ -7,7 +7,7 @@ pub fn register_default_metrics() {
 
 /// Record an API call for the given endpoint.
 pub fn record_api_call(endpoint: &str) {
-    counter!("api_calls_total", "endpoint" => endpoint.to_owned()).increment(1);
+    counter!("api_calls_total", "endpoint" => endpoint.to_owned());
 }
 
 /// Install the Prometheus exporter on the given port.
